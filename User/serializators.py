f31b16a8b706f05fd3090b6dd01e83618a23f175
JULFYKO <bkonarivskij@gmail.com>
2026-02-18 18:25:30 +0200

    Add User model with API endpoints

--- User/serializators.py
+++ User/serializators.py
@@ -10,8 +10,8 @@ class UserSerializer(serializers.ModelSerializer):
         }
 
     def create(self, validated_data):
+        password = validated_data.pop('password')
         user = User(**validated_data)
-        user.set_password(validated_data['password'])
+        user.set_password(password)
         user.save()
         return user
-
