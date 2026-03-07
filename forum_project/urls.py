3b6fad404f098866c31b3f7c850c3caee05cd39a
Anton Yurkovets <overgeardev@gmail.com>
2026-03-07 12:41:45 +0200

    Revert "user authentication"

--- forum_project/urls.py
+++ forum_project/urls.py
@@ -21,8 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include("Discussion.urls")),
-    path('api/', include("User.urls")),
+    path('api/', include("Discussion.urls"))
 ]
 
 if settings.DEBUG:
